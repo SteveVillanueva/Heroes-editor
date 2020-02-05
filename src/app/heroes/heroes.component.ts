41d63b3fcb8f1e743afcc0f36f6bece00d48b789
SteveVillanueva <36260033+SteveVillanueva@users.noreply.github.com>
2020-02-05 09:27:07 +0100

    Master/detail components done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,30 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-// imports hero class with data
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  // create a hero object
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  // exposes array for binding
+
   heroes = HEROES;
-  // onclick statement from heroes.component.html
   selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  constructor() {
-  }
+
+  constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
